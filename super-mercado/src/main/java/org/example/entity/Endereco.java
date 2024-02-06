d556f340a8e8062783969ce7646ce0ba70b333f1
joao vitor <jvtenorio@stefanini.com>
2024-02-06 17:03:41 -0300

    feat/Consultando Endereco pela api do cep e encapsulando classes

--- super-mercado/src/main/java/org/example/entity/Endereco.java
+++ super-mercado/src/main/java/org/example/entity/Endereco.java
@@ -8,13 +8,75 @@ public class Endereco {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_endereco")
     private Long id;
-    private Integer numero;
     private String cep;
     private String uf;
     private String logradouro;
     @ManyToOne
     @JoinColumn(name = "id_super_mercado")
     private SuperMercado idSupermercado;
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente idCliente;
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public SuperMercado getIdSupermercado() {
+        return idSupermercado;
+    }
+
+    public void setIdSupermercado(SuperMercado idSupermercado) {
+        this.idSupermercado = idSupermercado;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "id=" + id +
+                ", cep='" + cep + '\'' +
+                ", uf='" + uf + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", idSupermercado=" + idSupermercado +
+                '}';
+    }
 
     protected Endereco() {
     }
@@ -24,12 +86,13 @@ public class Endereco {
         public EnderecoBuild(Endereco endereco) {
             this.endereco = endereco;
         }
+
+        public EnderecoBuild() {
+
+        }
+
         public Endereco cep(String cep){
             endereco.cep = cep;
-            return endereco;
-        }
-        public Endereco numero(Integer numero){
-            endereco.numero = numero;
             return endereco;
         }
         public Endereco uf(String  uf){
