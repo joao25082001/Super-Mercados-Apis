1c1e0f4f3581d1b3e7b02e0d595fdde7111ecdbf
joao vitor <jvtenorio@stefanini.com>
2024-02-06 14:04:46 -0300

    fix/ finalizando crud da classe cliente

--- super-mercado/src/main/java/org/example/controller/ClienteController.java
+++ super-mercado/src/main/java/org/example/controller/ClienteController.java
@@ -1,9 +1,36 @@
 package org.example.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.DTO.Cliente.ClienteDTO;
+import org.example.DTO.Cliente.RequestAtualizacao;
+import org.example.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
+    @Autowired
+    ClienteService clienteService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<ClienteDTO> cadastrarCliente(@RequestBody ClienteDTO request) {
+        return ResponseEntity.ok().body(clienteService.cadastroCliente(request.getNome(), request.getDataNascimento(), request.getCpf(), request.getCep()));
+    }
+
+    @GetMapping("/exibir")
+    public ResponseEntity<List<ClienteDTO>> exibirUsuarios() {
+        return ResponseEntity.ok().body(clienteService.exibirUsuario());
+    }
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<RequestAtualizacao> atualizar(@PathVariable Long id,@RequestBody RequestAtualizacao request ) {
+        return ResponseEntity.ok().body(clienteService.atualizarUsuario(id,request));
+    }
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity deletar(@PathVariable Long id) {
+        clienteService.deletarUsuario(id);
+        return ResponseEntity.ok().build();
+    }
 }
