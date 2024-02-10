7f15ce953f9fd7eae7e9f55e007e6774555c24af
joao vitor <jvtenorio@stefanini.com>
2024-02-10 11:34:07 -0300

    refactor/ melhorando o data.sql

--- super-mercado/src/main/resources/data.sql
+++ super-mercado/src/main/resources/data.sql
@@ -3,26 +3,8 @@ VALUES ('Supermercado A', '123456789', 'supermercadoa@example.com');
 INSERT INTO super_mercado (nome, cnpj, email)
 VALUES ('Supermercado B', '123453789', 'supermercadoB@example.com');
 
-INSERT INTO endereco (logradouro, cep, uf, id_super_mercado)
-VALUES ('Rua Exemplo', '123','SP', 1);
 
 
-INSERT INTO lote (data_compra, quantidade_recebida)
-VALUES ('2023-01-01', 150);
-
-INSERT INTO lote (data_compra, quantidade_recebida)
-VALUES ('2023-02-05', 200);
-
-
-INSERT INTO lote (data_compra, quantidade_recebida)
-VALUES ('2023-03-10', 250);
-
-
-INSERT INTO lote (data_compra, quantidade_recebida)
-VALUES ('2023-04-15', 180);
-
-INSERT INTO lote (data_compra, quantidade_recebida)
-VALUES ('2023-05-20', 220);
 
 
 INSERT INTO produto (nome, marca, codigo, valor, quantidade_unidade, quantidade, id_super_mercado)
@@ -76,11 +58,26 @@ VALUES ('Papel Higiênico', 'Neve', 'PAH012', 5.99, 4, 40, 1);
 INSERT INTO produto (nome, marca, codigo, valor, quantidade_unidade, quantidade, id_super_mercado)
 VALUES ('Shampoo', 'Pantene', 'SHA345', 7.99, 2, 50, 1);
 
+INSERT INTO lote (data_compra, quantidade_recebida)
+VALUES ('2023-01-01', 150);
+
+INSERT INTO lote (data_compra, quantidade_recebida)
+VALUES ('2023-02-05', 200);
+
+
+INSERT INTO lote (data_compra, quantidade_recebida)
+VALUES ('2023-03-10', 250);
+
+
+INSERT INTO lote (data_compra, quantidade_recebida)
+VALUES ('2023-04-15', 180);
+
+INSERT INTO lote (data_compra, quantidade_recebida)
+VALUES ('2023-05-20', 220);
+
 
 INSERT INTO produto (nome, marca, codigo, valor, quantidade_unidade, quantidade, id_super_mercado)
 VALUES ('Desodorante', 'Rexona', 'DEO678', 3.49, 5, 70, 1);
-
-
 INSERT INTO controle_produto (id_lote, id_produto, data_vencimento)
 VALUES (NULL, 1, '2023-06-01');
 
@@ -132,35 +129,6 @@ VALUES (4, 15, '2023-06-10');
 INSERT INTO controle_produto (id_lote, id_produto, data_vencimento)
 VALUES (5, 16, '2023-07-20');
 
-INSERT INTO cliente (nome, data_nascimento, cpf) VALUES
-('João da Silva', '1990-05-15', '12345678910'),
-('Maria Oliveira', '1985-09-20', '98765432100'),
-('Pedro Santos', '1978-03-10', '11122233344'),
-('Ana Souza', '2000-12-25', '55566677788'),
-('Lucas Pereira', '1992-07-07', '99988877766'),
-('Carla Silva', '1983-11-30', '44433322211'),
-('Paulo Oliveira', '1975-02-18', '77788899900'),
-('Fernanda Santos', '1998-08-05', '22211100099'),
-('Rafaela Lima', '1980-06-12', '66655544433'),
-('Gustavo Almeida', '1993-04-03', '88899900011');
-
-
-
-INSERT INTO funcionario (nome, cpf, numero_carteira, id_super_mercado)
-VALUES ('João Silva', '12345678901', 'CT123', 1);
-
-INSERT INTO funcionario (nome, cpf, numero_carteira, id_super_mercado)
-VALUES ('Maria Santos', '98765432109', 'CT456', 1);
-
-INSERT INTO funcionario (nome, cpf, numero_carteira, id_super_mercado)
-VALUES ('Pedro Oliveira', '45678912345', 'CT789', 1);
-
-INSERT INTO funcionario (nome, cpf, numero_carteira, id_super_mercado)
-VALUES ('Ana Costa', '78912345678', 'CT012', 1);
-
-INSERT INTO caixa (numero, id_super_mercado, tipo_caixa, status)
-VALUES ('1', 1, 'PREFERENCIAL', 1);
-
 INSERT INTO caixa (numero, id_super_mercado, tipo_caixa, status)
 VALUES ('2', 1, 'RAPIDO', 1);
 
@@ -171,42 +139,6 @@ VALUES ('3', 1, 'COMUM', 0);
 INSERT INTO caixa (numero, id_super_mercado, tipo_caixa, status)
 VALUES ('4', 1, 'TOTEM', 1);
 
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (1, 1, '2023-06-01 09:00:00', '2023-06-01 18:00:00', 100.00, 150.00);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (2, 2, '2023-06-02 08:30:00', '2023-06-02 17:30:00', 150.00, null);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (null, 4, '2023-06-03 10:00:00', '2023-06-03 19:00:00', 200.00, 300.00);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (3, 3, '2023-06-04 09:30:00', '2023-06-04 18:30:00', 180.00, null);
-
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (1, 3, '2024-06-05 08:00:00', '2024-06-05 17:00:00', 120.00, 180.00);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (2, 2, '2023-12-06 09:30:00', '2023-12-06 18:30:00', 200.00, null);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (3, 1, '2022-06-07 10:00:00', '2022-06-07 19:00:00', 150.00, 250.00);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (null, 4, '2023-06-08 08:30:00', '2023-06-08 17:30:00', 180.00, null);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (4, 1, '2025-06-09 09:00:00', '2025-06-09 18:00:00', 160.00, 220.00);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (1, 2, '2023-06-10 07:30:00', '2023-06-10 16:30:00', 140.00, null);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (2,2, '2023-06-11 08:00:00', '2023-06-11 17:00:00', 200.00, 280.00);
-
-INSERT INTO jornada (id_funcionario, id_caixa, horario_abertura, horario_fechamento, valor_inicial, valor_final)
-VALUES (3, 1, '2023-06-12 09:30:00', '2023-06-12 18:30:00', 170.00, null);
 
 
 
